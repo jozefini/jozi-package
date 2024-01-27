8a199fbc789f58db7a0fe778d129602024e9eeaa
jozefini <jozibashaj@gmail.com>
2024-01-27 15:23:25 +0100

    chunk helpers

---
+++ .changeset/big-mirrors-kneel.md
@@ -0,0 +1,5 @@
+---
+"@jozefini/lib": major
+---
+
+Chunk helpers
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entryPoints: ["src/index.ts"],
+  entryPoints: [
+    "src/index.ts",
+    "src/object.ts",
+    "src/events.ts",
+    "src/formatting.ts",
+  ],
+  entry: {
+    index: "src/index.ts",
+    object: "src/object.ts",
+    events: "src/events.ts",
+    formatting: "src/formatting.ts",
+  },
   format: ["cjs", "esm"],
   dts: true,
   clean: true,
